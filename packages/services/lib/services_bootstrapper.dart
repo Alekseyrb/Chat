582d6f3f8a3776534955b70c1c7e44ee75a3cb03
Alekseyrb <kravchenko.a.a.rb.gmail.com>
2023-01-01 18:35:02 +0200

    add firestore services

--- packages/services/lib/services_bootstrapper.dart
+++ packages/services/lib/services_bootstrapper.dart
@@ -1,6 +1,9 @@
 import 'package:get_it/get_it.dart';
 import 'package:services/src/auth_service/auth_service.dart';
+import 'package:services/src/firestore_service/firestore_service.dart';
 
 Future<void> initServices(GetIt getIt) async {
   getIt.registerFactory(() => AuthService());
+
+  getIt.registerFactory(() => FirestoreService());
 }
